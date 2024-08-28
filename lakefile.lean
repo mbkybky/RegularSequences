c2ed1ac4110063d418adf99cd3560f8a0fd700c0
Hu Yongle <2065545849@qq.com>
2024-08-28 16:27:10 +0800

    init

---
+++ lakefile.lean
@@ -0,0 +1,22 @@
+import Lake
+open Lake DSL
+
+package "RegularSequences" where
+  -- add package configuration options here
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩
+  ]
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «RegularSequences» where
+  -- add library configuration options here
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
